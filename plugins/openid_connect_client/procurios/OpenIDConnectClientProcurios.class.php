6de8f5b09254d340cef73ac45e0e1688f15892a7
Thomas de Graaff <tdgraaff@sp.nl>
2020-11-12 17:25:31 +0100

    Initial commit.

---
+++ plugins/openid_connect_client/procurios/OpenIDConnectClientProcurios.class.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file
+ * Procurios OpenID Connect client.
+ */
+
+class OpenIDConnectClientProcurios extends OpenIDConnectClientBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm() {
+    $form = parent::settingsForm();
+
+    $default_site = 'https://example.com/oauth2';
+    $form['authorization_endpoint'] = array(
+      '#title' => t('Authorization endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->getSetting('authorization_endpoint', $default_site . '/authorize'),
+    );
+    $form['token_endpoint'] = array(
+      '#title' => t('Token endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->getSetting('token_endpoint', $default_site . '/token'),
+    );
+    $form['userinfo_endpoint'] = array(
+      '#title' => t('UserInfo endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->getSetting('userinfo_endpoint', $default_site . '/UserInfo'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEndpoints() {
+    return array(
+      'authorization' => $this->getSetting('authorization_endpoint'),
+      'token' => $this->getSetting('token_endpoint'),
+      'userinfo' => $this->getSetting('userinfo_endpoint'),
+    );
+  }
+}
